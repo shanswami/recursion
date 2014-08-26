2172967224825f3f21637d872d82272236a76710
Shan Swami <shan.swami@gmail.com>
2014-08-26 00:45:55 -0700

    implemented both recursion functions, tests passed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,17 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
-  // your code here
+var getElementsByClassName = function(className) {
+  var base = document.body;
+  var finalArray = [];
+  var checker = function(node) {
+    if (node.classList && node.classList.contains(className)) {
+      finalArray.push(node);
+    }
+    for (var i = 0; i < node.childNodes.length; i++) {
+      checker(node.childNodes[i]);
+    }
+  };
+  checker(base);
+  return finalArray;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,27 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+ var JSONstring = '';
+  if (typeof obj === 'function') {
+    return '';
+  } else if (typeof obj === 'string') {
+    return '"' + obj.replace(/\\/g, '\\\\').replace(/"/g, '\\"') + '"';
+  } else if (Array.isArray(obj)) {
+    JSONstring += '[';
+    JSONstring += obj.map(stringifyJSON).join(',');
+    JSONstring += ']';
+    return JSONstring;
+  } else if (obj != null && typeof obj === 'object') {
+    JSONstring += '{';
+    var arrr = [];
+    Object.keys(obj).forEach(function (key) {				// returns an array of a given object's properties
+      if (typeof obj[key] === 'undefined' || typeof obj[key] === 'function') {
+        return;
+      }
+      arrr.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    });
+    JSONstring += arrr.join(',');
+    JSONstring += '}';
+    return JSONstring;
+  }  return '' + obj;
 };
